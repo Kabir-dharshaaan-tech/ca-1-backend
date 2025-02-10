c0c80e324f6303fb9c7d49e50013ca08c93ea268
kabir-dharshaaan-tech <dharshaankabir@gmail.com>
2025-02-10 14:32:38 +0530

    done

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ca-1-backend
+
+
+render link:
+
+https://ca-1-backend-1.onrender.com
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app=express()
 app.use(express.json())
 
 app.get('/',(req,res)=>{
-    res.status("sucess")
+    res.send("sucess")
 })
 
 app.post('/signup',(req,res)=>{
