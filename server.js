fce857507f0c7a0b830bced0d65580ddcddcddcb
kabir-dharshaaan-tech <dharshaankabir@gmail.com>
2025-02-10 14:26:14 +0530

    done

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app=express()
 
 app.use(express.json())
 
+app.get('/',(req,res)=>{
+    res.status("sucess")
+})
+
 app.post('/signup',(req,res)=>{
     const {Username}=req.body
     const {Email}=req.body
